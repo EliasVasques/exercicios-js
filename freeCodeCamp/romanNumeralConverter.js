edd0eafca3b62c34db471d14693fc9ce7dd28735
EliasVasques <eliasvasques74@gmail.com>
2022-01-21 08:40:50 -0300

    romano finalizado

--- freeCodeCamp/romanNumeralConverter.js
+++ freeCodeCamp/romanNumeralConverter.js
@@ -1,26 +1,5 @@
+/* 1 solução
 function convertToRoman(num) {
-    /*
-    let table = ['I', 'V', 'X', 'L', 'C', 'D', 'M']; 
-    let arr = String(num).split('');
-    //console.log('I'.repeat(6 - 5))
-    let inRoman = ''; 
-  
-    for (let num of arr) {
-      switch(arr.length) {
-        case 1:
-          if (num <= 3) inRoman += 'I'.repeat(3);
-          else if (num == 4) inRoman += 'IV';
-          else if (num => 5 && num <= 8) {
-            inRoman += 'V' +  'I'.repeat(num - 5);
-          } 
-          else if (num == 9) inRoman += 'IX';
-          break
-      }
-    }
-    console.log(inRoman)
-  
-   return num;*/
-    //num = 3999
     let inRoman = '';
     let arr = String(num).split('');
     const values = {
@@ -48,37 +27,51 @@ function convertToRoman(num) {
       }
       num = Number(arr[0] + '0'.repeat(arr.length - 1)); 
       arr.shift()
-      //console.log(num)
-  
+
       let sum = 0;
       for (let sign in values) {
-        //console.log(sign)
         if (values[sign] <= num) {
           while (sum + values[sign] <= num) {
             inRoman += sign;
             sum += values[sign];
-            //console.log(sum)
           }
         }
       }
   
     }
-    /*let name = 'Elias'
-    name.replace('E', 'i')
-    console.log(name)*/
-  
-    // substituir 4 seguidos
     for (let sign in fourInARow) {
-      //console.log(sign)
       if (inRoman.includes(sign)) {
-        //console.log(sign.repeat(4))
-        //if (sign === 'I') 
         inRoman = inRoman.replace(sign, fourInARow[sign]);
       }
     }
-  
-    console.log(inRoman)
     return inRoman;
+}*/
+
+// 2 solução
+function convertToRoman(num) {
+  let inRoman = '';
+  const values = {
+    M: 1000,
+    CM: 900,
+    D: 500, 
+    CD: 400,
+    C: 100, 
+    XC: 90,
+    L: 50, 
+    XL: 40,
+    X: 10, 
+    IX: 9,
+    V: 5,
+    IV: 4, 
+    I: 1, 
+  }
+  for (let sign in values) {
+    while (num >= values[sign]) {
+      inRoman += sign;
+      num -= values[sign];
+    } 
+  }
+  return inRoman;
 }
-  
+
 convertToRoman(36);
